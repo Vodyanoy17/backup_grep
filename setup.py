157a9e6df990110e3095bb220c8980575048810f
Gregory Vinopal <35487262+Vodyanoy17@users.noreply.github.com>
2023-11-06 15:25:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_project',
+    version='1.0.0',
+    description='My project',
+    author='Your name',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+    install_requires=['dependency1', 'dependency2'],
+)
 
